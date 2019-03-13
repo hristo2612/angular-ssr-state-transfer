5d2b898ab0d94be2c9db49cd7d2da22d3f516460
hristo2612 <hristoapps@gmail.com>
2019-03-13 17:06:20 +0200

    Add HttpModule & call simple free api
    -the idea here is to achieve server side api calling and client side calling of the API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  dogs: any;
+
+  constructor( private http: HttpClient ) {
+
+  }
+
+  ngOnInit() {
+    this.http.get('https://dog.ceo/api/breeds/list/all').subscribe((data) => {
+      console.log(data);
+      this.dogs = data;
+    })
+  }
 }
